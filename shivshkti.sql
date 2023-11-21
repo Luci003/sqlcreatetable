97d90ab9d921f4e88496f66ef131e3332db0261a
Vinayak Patil <87493541+Luci003@users.noreply.github.com>
2023-11-21 15:46:34 +0530

    Update shivshkti.sql

--- shivshkti.sql
+++ shivshkti.sql
@@ -1,1 +1,16 @@
 
+create table Shivshkti  (
+	id integer,
+    empname text,
+    address text
+   );
+insert into shivshkti (id, empname, address) values (1, 'Vinayak Patil', 'Kolhapur');
+insert into shivshkti (id, empname, address) values (2, 'Suraj Patil','Pune');
+insert into shivshkti (id, empname, address) values (3, 'Vijay Lokhande','Mumbai');
+// Add new column
+alter table shivshkti add column email taxt;
+
+
+update shivshkti set email='vinayakpatil@gmail.com' where id=1;
+update shivshkti set email='surajpatil@gmail.com' where id=2;
+update shivshkti set email='vijaylokhande@gmail.com' where id=3;
